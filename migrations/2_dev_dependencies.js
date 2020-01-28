9c737dce70f3ec8aa30b0098e638bbfd8cc26e2a
josojo <josojo@hotmail.de>
2020-01-28 19:57:16 +0530

    felix comments on PR https://github.com/gnosis/dex-contracts/pull/500/files

--- migrations/2_dev_dependencies.js
+++ migrations/2_dev_dependencies.js
@@ -1,10 +1,26 @@
 const migrateDependencies = require("@gnosis.pm/dex-contracts/src/migration/dependencies");
+const migrateBatchExchange = require("@gnosis.pm/dex-contracts/src/migration/PoC_dfusion");
 
-module.exports = function(deployer, network, accounts) {
-  return migrateDependencies({
-    artifacts,
-    deployer,
-    network,
-    accounts
-  });
+module.exports = async function(deployer, network, accounts) {
+  if (network === "development") {
+    await migrateDependencies({
+      artifacts,
+      deployer,
+      network,
+      account: accounts[0]
+    });
+    return migrateBatchExchange({
+      artifacts,
+      deployer,
+      network,
+      account: accounts[0],
+      web3
+    });
+  } else {
+    // eslint-disable-next-line no-console
+    console.log(
+      "Not in development, so nothing to do. Current network is %s",
+      network
+    );
+  }
 };
