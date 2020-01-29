46e2a6ec113a406383f2c06293229e1da11d88d7
josojo <josojo@hotmail.de>
2020-01-29 22:09:29 +0530

    testing

--- migrations/2_dev_dependencies.js
+++ migrations/2_dev_dependencies.js
@@ -1,14 +1,7 @@
-const migrateDependencies = require("@gnosis.pm/dex-contracts/src/migration/dependencies");
 const migrateBatchExchange = require("@gnosis.pm/dex-contracts/src/migration/PoC_dfusion");
 
 module.exports = async function(deployer, network, accounts) {
   if (network === "development") {
-    await migrateDependencies({
-      artifacts,
-      deployer,
-      network,
-      account: accounts[0]
-    });
     return migrateBatchExchange({
       artifacts,
       deployer,
