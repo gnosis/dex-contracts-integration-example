daaf6882fd397137b7c7b272057b2ad0486148b7
josojo <josojo@hotmail.de>
2020-01-30 14:28:47 +0530

    integrating BiMap and IterableAppendonlyLibrary

--- contracts/DevDependencies.sol
+++ contracts/DevDependencies.sol
@@ -8,5 +8,7 @@ pragma solidity ^0.5.0;
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
 import "@gnosis.pm/owl-token/contracts/TokenOWLProxy.sol";
 import "@gnosis.pm/owl-token/contracts/TokenOWL.sol";
+import "@gnosis.pm/solidity-data-structures/contracts/libraries/IdToAddressBiMap.sol";
+import "@gnosis.pm/solidity-data-structures/contracts/libraries/IterableAppendOnlySet.sol";
 
 contract DevDependencies {}
